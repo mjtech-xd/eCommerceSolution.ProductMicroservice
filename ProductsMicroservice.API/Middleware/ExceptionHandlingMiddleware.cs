b6836a04d2103c48207f295d64a23c7b900a2d11
Manoj Kumar Jaiswal <75155083+mjtech-xd@users.noreply.github.com>
2026-05-20 12:14:11 +0530

    Add initial middleware, dependency injection, and configuration for logging (#1)

---
+++ ProductsMicroservice.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,31 @@
+namespace ProductsMicroservice.API.Middleware;
+
+public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            //Log the exception type and message
+            logger.LogError($"{ex.GetType().ToString()}:{ex.Message}");
+            if(ex.InnerException is not null)
+            {
+                logger.LogError($"Inner Exception: {ex.InnerException.GetType().ToString()}:{ex.InnerException.Message}");
+            }
+
+            httpContext.Response.StatusCode = 500; // Internal Server Error
+            await httpContext.Response.WriteAsJsonAsync(new {Message = ex.Message, Type = ex.GetType().ToString()});
+        }
+    }
+}
+public static class ExceptionHandlingMiddlewareExtensions
+{
+    public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ExceptionHandlingMiddleware>();
+    }
+}
