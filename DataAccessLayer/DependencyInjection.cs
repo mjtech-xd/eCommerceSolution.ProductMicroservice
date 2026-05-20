e217faeed1206e84ece9d9484415d9baa8252113
Manoj Jaiswal <manoj.jaiswal883@gmail.com>
2026-05-20 14:00:15 +0530

    Implement data access layer with Entity Framework Core and PostgreSQL integration

--- DataAccessLayer/DependencyInjection.cs
+++ DataAccessLayer/DependencyInjection.cs
@@ -1,12 +1,22 @@
+using DataAccessLayer.Context;
+using DataAccessLayer.Repository;
+using DataAccessLayer.RepositoryContracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DataAccessLayer;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddDataAccessLayer(this IServiceCollection services)
+    public static IServiceCollection AddDataAccessLayer(this IServiceCollection services,  IConfiguration configuration)
     {
         //Todo :Add DataAccessLayer into the IOC container
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        });
+        services.AddScoped<IProductRepository, ProductsRepository>();
         return services;
     }
 }
