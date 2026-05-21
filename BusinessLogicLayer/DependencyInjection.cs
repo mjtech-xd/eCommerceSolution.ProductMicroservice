b65376d6a20338a57f1aef7db238c074a756ef62
Manoj Kumar Jaiswal <75155083+mjtech-xd@users.noreply.github.com>
2026-05-21 21:33:01 +0530

    temp1 (#6)

--- BusinessLogicLayer/DependencyInjection.cs
+++ BusinessLogicLayer/DependencyInjection.cs
@@ -7,7 +7,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddDataBusinessLogicLayer(this IServiceCollection services)
     {
-        
+        // temp 1 commit
         services.AddAutoMapper(cfg => { }, typeof(ProductAddRequestToProductMappingProfile));
         return services;
     }
