060bd6fcc0b2763fd63331c7d6a0229a8ee5da2f
Manoj Kumar Jaiswal <75155083+mjtech-xd@users.noreply.github.com>
2026-06-08 14:03:58 +0530

    Fix database (#8)
    * Enhance database configuration and product entity mapping for PostgreSQL
    
    * fix

--- ProductsMicroservice.API/Program.cs
+++ ProductsMicroservice.API/Program.cs
@@ -14,6 +14,20 @@ builder.Services.AddDataBusinessLogicLayer();
 builder.Services.AddControllers();
 builder.Services.AddFluentValidationAutoValidation();
 
+//Swagger 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Cors
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder => 
+    {
+        builder.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 var app = builder.Build();
 
 // Apply database migrations on startup
@@ -22,9 +36,12 @@ using (var scope = app.Services.CreateScope())
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     dbContext.Database.Migrate();
 }
-
+//Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseExceptionHandlingMiddleware();
 app.UseRouting();
+app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
